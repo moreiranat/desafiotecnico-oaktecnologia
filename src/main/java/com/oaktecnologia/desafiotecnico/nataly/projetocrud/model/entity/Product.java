a68aeb4a768d6e108baaae7ef6f30ff81aaf9795
Nataly Lucena <natalylucena.pb@gmail.com>
2023-05-28 20:55:36 -0300

    Excluindo a dependência do Lombok e adicionando os getters e setters na entidade e no DTO

--- src/main/java/com/oaktecnologia/desafiotecnico/nataly/projetocrud/model/entity/Product.java
+++ src/main/java/com/oaktecnologia/desafiotecnico/nataly/projetocrud/model/entity/Product.java
@@ -2,6 +2,7 @@ package com.oaktecnologia.desafiotecnico.nataly.projetocrud.model.entity;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.Objects;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -9,14 +10,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "TB_PRODUCT")
 public class Product implements Serializable {
 	
@@ -38,5 +33,74 @@ public class Product implements Serializable {
 	
 	@Column(name = "PRODUCT_AVAILABLEFORSALE", nullable = false)
 	private Boolean availableForSale = false;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getValue() {
+		return value;
+	}
+
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+
+	public Boolean getAvailableForSale() {
+		return availableForSale;
+	}
+
+	public void setAvailableForSale(Boolean availableForSale) {
+		this.availableForSale = availableForSale;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(availableForSale, description, id, name, value);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(availableForSale, other.availableForSale)
+				&& Objects.equals(description, other.description) && Objects.equals(id, other.id)
+				&& Objects.equals(name, other.name) && Objects.equals(value, other.value);
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", description=" + description + ", value=" + value
+				+ ", availableForSale=" + availableForSale + "]";
+	}
 
 }
--- src/main/java/com/oaktecnologia/desafiotecnico/nataly/projetocrud/presentation/dto/ProductDTO.java
+++ src/main/java/com/oaktecnologia/desafiotecnico/nataly/projetocrud/presentation/dto/ProductDTO.java
@@ -2,13 +2,6 @@ package com.oaktecnologia.desafiotecnico.nataly.projetocrud.presentation.dto;
 
 import java.math.BigDecimal;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class ProductDTO {
 	
 	private Long id;
@@ -21,4 +14,44 @@ public class ProductDTO {
 	
 	private Boolean availableForSale = false;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getValue() {
+		return value;
+	}
+
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+
+	public Boolean getAvailableForSale() {
+		return availableForSale;
+	}
+
+	public void setAvailableForSale(Boolean availableForSale) {
+		this.availableForSale = availableForSale;
+	}
+	
 }
