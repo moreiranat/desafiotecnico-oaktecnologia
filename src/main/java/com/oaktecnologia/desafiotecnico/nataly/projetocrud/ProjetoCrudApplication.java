6e63e0e54904ed0e29c39487d4edc64c4e376da8
Nataly Lucena <natalylucena.pb@gmail.com>
2023-05-28 01:13:15 -0300

    Criando o projeto e Adicionando as dependências

---
+++ src/main/java/com/oaktecnologia/desafiotecnico/nataly/projetocrud/ProjetoCrudApplication.java
@@ -0,0 +1,13 @@
+package com.oaktecnologia.desafiotecnico.nataly.projetocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCrudApplication.class, args);
+	}
+
+}
