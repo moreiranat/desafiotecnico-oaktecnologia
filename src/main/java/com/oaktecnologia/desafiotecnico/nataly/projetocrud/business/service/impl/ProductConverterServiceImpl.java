d6b1978c7a3f58a2af836767a55629c48ccfb869
Nataly Lucena <natalylucena.pb@gmail.com>
2023-05-28 22:26:37 -0300

    Implementando os métodos de conversão em ProductConverterServiceImpl

--- src/main/java/com/oaktecnologia/desafiotecnico/nataly/projetocrud/business/service/impl/ProductConverterServiceImpl.java
+++ src/main/java/com/oaktecnologia/desafiotecnico/nataly/projetocrud/business/service/impl/ProductConverterServiceImpl.java
@@ -1,5 +1,6 @@
 package com.oaktecnologia.desafiotecnico.nataly.projetocrud.business.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -11,28 +12,80 @@ import com.oaktecnologia.desafiotecnico.nataly.projetocrud.presentation.dto.Prod
 @Service
 public class ProductConverterServiceImpl implements ProductConverterService {
 
+	private static final String MESSAGE = "Could not convert, because object is null";
+
 	@Override
 	public Product dtoToProduct(ProductDTO dto) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		if (dto != null) {
+            Product entity = new Product();
+
+            entity.setId(dto.getId());
+            entity.setName(dto.getName());
+            entity.setDescription(dto.getDescription());
+            entity.setValue(dto.getValue());
+            entity.setAvailableForSale(dto.getAvailableForSale());
+
+            return entity;
+        }
+		
+        throw new IllegalArgumentException(MESSAGE);
 	}
 
 	@Override
 	public ProductDTO productToDTO(Product entity) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		if (entity != null) {
+            ProductDTO dto = new ProductDTO();
+
+            dto.setId(entity.getId());
+            dto.setName(entity.getName());
+            dto.setDescription(entity.getDescription());
+            dto.setValue(entity.getValue());
+            dto.setAvailableForSale(entity.getAvailableForSale());
+
+            return dto;
+        }
+		
+        throw new IllegalArgumentException(MESSAGE);
 	}
 
 	@Override
 	public List<ProductDTO> productToDTOList(List<Product> entities) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		if(entities != null) {
+            List<ProductDTO> dtos = new ArrayList<>();
+
+            if(!entities.isEmpty()) {
+                for (Product product: entities) {
+                    ProductDTO dto = productToDTO(product);
+                    dtos.add(dto);
+                }
+            }
+
+            return dtos;
+        }
+		
+        throw new IllegalArgumentException(MESSAGE);
 	}
 
 	@Override
 	public List<Product> dtoToProductList(List<ProductDTO> dtos) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		
+		if(dtos != null) {
+            List<Product> entities = new ArrayList<>();
+
+            if(!dtos.isEmpty()) {
+                for (ProductDTO dto : dtos) {
+                   Product entity = dtoToProduct(dto);
+                    entities.add(entity);
+                }
+            }
+
+            return entities;
+        }
+		
+        throw new IllegalArgumentException(MESSAGE);
+    }
 
 }
